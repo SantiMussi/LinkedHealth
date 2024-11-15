f907b4ab8834b468ad4693eb8ef306b0e03226b4
Santiago Mussi <correagameroficial@gmail.com>
2024-11-15 09:55:39 -0300

    Added commands

--- src/main/java/LH/sanju/Main.java
+++ src/main/java/LH/sanju/Main.java
@@ -2,6 +2,9 @@ package LH.sanju;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,21 +15,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+    private boolean isPluginEnabled = true;
     public static String prefix = ChatColor.GRAY + "[" + ChatColor.RED + "LinkedHealth" + ChatColor.GRAY + "] ";
     private String version = getDescription().getVersion();
+
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info(ChatColor.translateAlternateColorCodes('&', prefix + "&aPlugin enabled! Version: " + version));
+        Bukkit.getConsoleSender().sendMessage((ChatColor.translateAlternateColorCodes('&', prefix + "&aPlugin enabled! Version: " + version)));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(ChatColor.translateAlternateColorCodes('&', prefix+ "Plugin disabled!"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', prefix+ "Plugin disabled!"));
     }
 
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent event) {
+        if(!isPluginEnabled) return;
         if (event.getEntity() instanceof Player damagedPlayer) {
             double damage = event.getFinalDamage();
             double newHealth = Math.max(0, damagedPlayer.getHealth() - damage);
@@ -44,6 +50,7 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if(!isPluginEnabled) return;
         Player newPlayer = event.getPlayer();
         double referenceHealth = 20.0; // Default health if no players are online
 
@@ -62,6 +69,7 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerRegainHealth(EntityRegainHealthEvent event) {
+        if(!isPluginEnabled) return;
         // Check if the entity is a player
         if (event.getEntity() instanceof Player healedPlayer) {
 
@@ -81,6 +89,89 @@ public class Main extends JavaPlugin implements Listener {
                     player.setHealth(Math.min(player.getMaxHealth(), newHealth));
                 }
             }
+
+            if(newHealth == 20.0){
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_WORK_CLERIC, 1.0f, 1.0f);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&aAll players are now at full health!"));
+                }
+            }
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("linkedhealth")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§aLinkedHealthPlugin v"+ version + " by "+ getDescription().getAuthors() + " - Use /linkedhealth help for commands."));
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("help")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§aLinkedHealth Commands:"));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§e/linkedhealth help §7- Show this help message."));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§e/sethealth <value> §7- Set the health for all players."));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§e/syncHealth §7- Enable or disable the plugin."));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§e/isSyncHealth §7- Shows if the plugin is enabled or disabled."));
+                return true;
+            }
+        }
+
+        //Command to set health for all players
+        if (command.getName().equalsIgnoreCase("sethealth")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§cOnly players can use this command."));
+                return true;
+            }
+
+            if(!sender.hasPermission("linkedhealth.commands.sethealth")){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cYou don't have permission to use this command!"));
+                return true;
+            }
+
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§cUsage: /sethealth <value>"));
+                return true;
+            }
+
+            try {
+                double health = Double.parseDouble(args[0]);
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.setHealth(Math.min(player.getMaxHealth(), health));
+                }
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§aHealth set to " + health + " for all players."));
+            } catch (NumberFormatException e) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "§cInvalid number. Please provide a valid health value."));
+            }
+            return true;
+        }
+
+        //Command to enable or disable the plugin
+        if(command.getName().equalsIgnoreCase("syncHealth")){
+            if(!sender.hasPermission("linkedhealth.commands.sync")){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cYou don't have permission to use this command!"));
+                return true;
+            }
+            if(isPluginEnabled){
+                isPluginEnabled = false;
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cPlugin disabled!"));
+            } else {
+                isPluginEnabled = true;
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&aPlugin enabled!"));
+            }
+            return true;
+        }
+
+        //Command to check if the plugin is enabled or disabled
+        if(command.getName().equalsIgnoreCase("isSyncHealth")){
+            if(isPluginEnabled){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&aPlugin is enabled!"));
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cPlugin is disabled!"));
+            }
+            return true;
+        }
+
+        return false; // If the command doesn't match any known commands, return false.
     }
 }
